1614613390c57f712d61cd203cf66cd4970fe418
travishuff <huff.travis@gmail.com>
2020-04-25 08:48:58 -0700

    Fix  lint

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useFetchVirusStats } from '../hooks/useFetchVirusStats'
 import StatTable from './StatTable'
 import '../App.css';
